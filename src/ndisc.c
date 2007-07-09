1032babdd6867e5b71f988a78774c01cf75c5f3b
Rémi Denis-Courmont <remi@remlab.net>
2007-07-09 20:02:36 +0000

    Switch to GLPv3

--- src/ndisc.c
+++ src/ndisc.c
@@ -3,21 +3,20 @@
  * $Id$
  */
 
-/***********************************************************************
- *  Copyright (C) 2004-2006 Rémi Denis-Courmont.                       *
- *  This program is free software; you can redistribute and/or modify  *
- *  it under the terms of the GNU General Public License as published  *
- *  by the Free Software Foundation; version 2 of the license.         *
- *                                                                     *
- *  This program is distributed in the hope that it will be useful,    *
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of     *
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               *
- *  See the GNU General Public License for more details.               *
- *                                                                     *
- *  You should have received a copy of the GNU General Public License  *
- *  along with this program; if not, you can get it from:              *
- *  http://www.gnu.org/copyleft/gpl.html                               *
- ***********************************************************************/
+/*************************************************************************
+ *  Copyright © 2004-2006 Rémi Denis-Courmont.                           *
+ *  This program is free software: you can redistribute and/or modify    *
+ *  it under the terms of the GNU General Public License as published by *
+ *  the Free Software Foundation, version 3 of the license.              *
+ *                                                                       *
+ *  This program is distributed in the hope that it will be useful,      *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ *  GNU General Public License for more details.                         *
+ *                                                                       *
+ *  You should have received a copy of the GNU General Public License    *
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>. *
+ *************************************************************************/
 
 #ifdef HAVE_CONFIG_H
 # include <config.h>
