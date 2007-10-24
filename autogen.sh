807ac3845dfdb1b188ad6ba1f197138c1d791eee
Rémi Denis-Courmont <remi@remlab.net>
2007-10-24 17:29:48 +0000

    Simplify through autoreconf

--- autogen.sh
+++ autogen.sh
@@ -25,43 +25,17 @@ fi
 echo "Creating admin directory ..."
 test -d admin || mkdir admin || exit 1
 
-echo "Running \`autopoint' ..."
-autopoint -f || {
-echo "Error: gettext is probably not on your system, or it does not work."
-echo "You need GNU gettext version 0.12.1 or higher."
-exit 1
-}
-
+echo "Running autoreconf ..."
+autoreconf -sfi
 unlink po/Makevars.template
 
-gettext_h=""
 for d in /usr /usr/local /opt/gettext /usr/pkg $HOME ; do
 	if test -f $d/share/gettext/gettext.h ; then
-		test -z "$gettext_h" && ln -sf $d/share/gettext/gettext.h \
-					include/gettext.h
-		gettext_h=ok
+		ln -sf $d/share/gettext/gettext.h include/gettext.h
 	fi
 done
 
-echo "Generating \`aclocal.m4' with aclocal ..."
-aclocal -I m4 || {
-echo "Error: autoconf is probably not on your system, or it does not work."
-echo "You need GNU autoconf 2.59c or higher, as well as GNU gettext 0.12.1."
-exit 1
-}
-echo "Generating \`config.h.in' with autoheader ..."
-autoheader || exit 1
-echo "Generating \`Makefile.in' with automake ..."
-automake -Wall --add-missing || {
-echo "Error: automake is probably not on your system, or it is too old."
-echo "You need GNU automake 1.7 higher to rebuild this package."
-exit 1
-}
-echo "Generating \`configure' script with autoconf ..."
-autoconf || exit 1
-echo "Done."
-
-test -z $gettext_h && {
+test -f "include/gettext.h" || {
 echo "Error: can't find <gettext.h> convenience C header."
 echo "Please put a link to it by hand as include/gettext.h"
 }
