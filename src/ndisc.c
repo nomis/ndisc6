2be32a5fc4b5214f6c9074c464e57a556fcf9e2e
Rémi Denis-Courmont <remi@remlab.net>
2011-02-20 13:38:25 +0000

    Add RA proxy bit (RFC4389)

--- src/ndisc.c
+++ src/ndisc.c
@@ -436,6 +436,8 @@ parseadv (const uint8_t *buf, size_t len, const struct sockaddr_in6 *tgt,
 		printf (_("Mobile home agent         :          %3s\n"),
 		        gettext ((v & ND_RA_FLAG_HOME_AGENT) ? N_ ("Yes") : N_("No")));
 		printf (_("Router preference         :       %6s\n"), pref_i2n (v));
+		printf (_("Neighbor discovery proxy  :          %3s\n"),
+		        gettext ((v & 0x04) ? N_ ("Yes") : N_("No")));
 
 		/* Router lifetime */
 		fputs (_("Router lifetime           : "), stdout);
